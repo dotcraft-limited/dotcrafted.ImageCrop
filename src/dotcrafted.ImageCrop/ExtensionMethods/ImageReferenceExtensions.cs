2c81c7b28dca57bc8d93b64d68fdac31063d79d7
Maciej Golis <maciej.golis@gmail.com>
2021-03-03 21:00:03 +0000

    Problems with imported content #5

--- src/dotcrafted.ImageCrop/ExtensionMethods/ImageReferenceExtensions.cs
+++ src/dotcrafted.ImageCrop/ExtensionMethods/ImageReferenceExtensions.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Web.Mvc;
 using EPiServer;
+using EPiServer.Core;
 using EPiServer.Logging;
+using EPiServer.ServiceLocation;
+using EPiServer.Web.Mvc.Html;
 using EPiServer.Web.Routing;
 using ImageResizer.Plugins.EPiServer;
 using ITMeric.ImageCrop.Core;
@@ -10,25 +14,34 @@ namespace ITMeric.ImageCrop.ExtensionMethods
     public static class ImageReferenceExtensions
     {
         private static readonly ILogger _logger;
+        private static readonly IContentLoader _contentLoader;
+        private static readonly UrlHelper _urlHelper;
 
         static ImageReferenceExtensions()
         {
             _logger = LogManager.GetLogger(typeof(ImageReferenceExtensions));
+            _contentLoader = ServiceLocator.Current.GetInstance<IContentLoader>();
+            _urlHelper = ServiceLocator.Current.GetInstance<UrlHelper>();
         }
 
         public static UrlBuilder GetCropUrl(this ImageReference imageReference, int? width = null, int? height = null,
             string fallback = null)
         {
+            
             if (imageReference == null)
                 return GetFallback(fallback);
 
+            var image = _contentLoader.Get<ImageData>(imageReference.ContentReferenceId);
+
+            if (image == null)
+                return GetFallback(fallback);
+
             try
-            {
-                var url = UrlResolver.Current.GetUrl(new EPiServer.Core.ContentReference(imageReference.ContentLink));
+            {                 
+                var url = _urlHelper.ContentUrl(image.ContentLink);
 
                 if (string.IsNullOrEmpty(url))
                     throw new Exception("Could not retrieve image's url");
-
 
                 var urlBuilder = new UrlBuilder(url);
 
